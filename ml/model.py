6af426a26fdcd3ad2f4bb527b23c1cdda5bac4a6
JoQri <rbfl7566@gmail.com>
2024-07-14 16:51:09 +0900

    20240714

--- ml/model.py
+++ ml/model.py
@@ -1,126 +1,59 @@
-from sklearn.model_selection import GridSearchCV, train_test_split, cross_val_score
+from sklearn.model_selection import GridSearchCV
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, AdaBoostRegressor
 from sklearn.neighbors import KNeighborsRegressor
 from xgboost import XGBRegressor
 from lightgbm import LGBMRegressor
-
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-import numpy as np
 
-def grid_search(X, y, model_name):
-    if model_name == "DecisionTreeRegressor":
-        model = DecisionTreeRegressor(random_state=13)
-        param_grid = {
-            'max_depth': [3, 5, 7, 10],
-            'min_samples_split': [10, 20, 25, 30]
-        }
-    elif model_name == "RandomForestRegressor":
-        model = RandomForestRegressor(random_state=13)
-        param_grid = {
-            'n_estimators': [100, 200, 300, 350],
-            'max_depth': [3, 5, 7, 10],
-            'min_samples_split': [10, 15, 20, 30]
-        }
-    elif model_name == "XGBoostRegressor":
-        model = XGBRegressor(random_state=13)
-        param_grid = {
-            'n_estimators': [50, 100, 200],
-            'max_depth': [3, 5, 7],
-            'learning_rate': [0.05, 0.1, 0.2]
-        }
-    elif model_name == "LightGBM":
-        model = LGBMRegressor(random_state=13)
-        param_grid = {
-            'n_estimators': [50, 100, 200, 250],
-            'max_depth': [3, 5, 7],
-            'learning_rate': [0.05, 0.1, 0.2, 0.3]
-        }
-    elif model_name == "GradientBoostingRegressor":
-        model = GradientBoostingRegressor(random_state=13)
-        param_grid = {
-            'n_estimators': [50, 75, 100, 200, 250],
-            'max_depth': [3, 5, 7, 10],
-            'learning_rate': [0.05, 0.1, 0.2, 0.3]
-        }
-    elif model_name == "AdaBoostRegressor":
-        model = AdaBoostRegressor(random_state=13)
-        param_grid = {
-            'n_estimators': [30, 50, 100, 200],
-            'learning_rate': [0.03, 0.05, 0.1, 0.2]
-        }
-    elif model_name == "KNeighborsRegressor":
-        model = KNeighborsRegressor()
-        param_grid = {
-            'n_neighbors': [5, 10, 15, 20, 30],
-            'weights': ['uniform', 'distance']
-        }
-    else:
-        raise ValueError("Invalid model name")
+# 모델과 파라미터 정의
+models = {
+    'DecisionTreeRegressor': (DecisionTreeRegressor(random_state=13),
+                              {'max_depth': [3, 5, 7, 10],
+                               'min_samples_split': [10, 20, 25, 30]}),
     
-    # 모델평가는 MAE
+    'RandomForestRegressor': (RandomForestRegressor(random_state=13),
+                              {'n_estimators': [100, 200, 300, 350],
+                               'max_depth': [3, 5, 7, 10],
+                               'min_samples_split': [10, 15, 20, 30]}),
+    
+    'XGBRegressor': (XGBRegressor(random_state=13),
+                     {'n_estimators': [50, 100, 200],
+                      'max_depth': [3, 5, 7],
+                      'learning_rate': [0.05, 0.1, 0.2]}),
+    
+    'LGBMRegressor': (LGBMRegressor(random_state=13),
+                      {'n_estimators': [50, 100, 200, 250],
+                       'max_depth': [3, 5, 7],
+                       'learning_rate': [0.05, 0.1, 0.2, 0.3]}),
+    
+    'GradientBoostingRegressor': (GradientBoostingRegressor(random_state=13),
+                                  {'n_estimators': [50, 75, 100, 200, 250],
+                                   'max_depth': [3, 5, 7, 10],
+                                   'learning_rate': [0.05, 0.1, 0.2, 0.3]}),
+    
+    'AdaBoostRegressor': (AdaBoostRegressor(random_state=13),
+                          {'n_estimators': [30, 50, 100, 200],
+                           'learning_rate': [0.03, 0.05, 0.1, 0.2]}),
+    
+    'KNeighborsRegressor': (KNeighborsRegressor(),
+                            {'n_neighbors': [5, 10, 15, 20, 30],
+                             'weights': ['uniform', 'distance']})
+}
+
+# 그리드 서치 함수
+def grid_search(X, y, model, param_grid):
     grid_search = GridSearchCV(model, param_grid, cv=5, scoring='neg_mean_absolute_error')
     grid_search.fit(X, y)
-    
     return grid_search.best_estimator_, grid_search.best_params_
 
-# 모델평가
+# 모델 평가 함수
 def evaluate(model, X, y):
-    # 예측값 계산
     y_pred = model.predict(X)
-    
-    # MAE 계산
     mae = mean_absolute_error(y, y_pred)
-    print("MAE:", mae)
-    
-    # MSE 계산
     mse = mean_squared_error(y, y_pred)
-    print("MSE:", mse)
-
-    # R^2 계산
     r2 = r2_score(y, y_pred)
+    print("MAE:", mae)
+    print("MSE:", mse)
     print("R^2:", r2)
-    
     return mae, mse, r2
-
-# 각각의 모델
-def tr_dt(X, y, max_depth=None, min_samples_split=2):
-    model = DecisionTreeRegressor(max_depth=max_depth, min_samples_split=min_samples_split, random_state=13)
-    model.fit(X, y)
-    return model
-
-def tr_rf(X, y, n_estimators=100, max_depth=None, min_samples_split=2):
-    model = RandomForestRegressor(n_estimators=n_estimators, max_depth=max_depth, min_samples_split=min_samples_split, random_state=13)
-    model.fit(X, y)
-    return model
-
-def tr_knn(X, y, n_neighbors=5, weights='uniform'):
-    model = KNeighborsRegressor(n_neighbors=n_neighbors, weights=weights)
-    model.fit(X, y)
-    return model
-
-def tr_xgb(X, y, n_estimators=50, max_depth=None, learning_rate=0.05):
-    model = XGBRegressor(n_estimators=n_estimators, max_depth=max_depth, learning_rate=learning_rate, random_state=13)
-    model.fit(X, y)
-    return model
-
-def tr_ada(X, y, n_estimators=50, learning_rate=0.1):
-    model = AdaBoostRegressor(n_estimators=n_estimators, learning_rate=learning_rate, random_state=13)
-    model.fit(X, y)
-    return model
-
-def tr_lgb(X, y, max_depth=2, learning_rate=0.1, n_estimators=100):
-    model = LGBMRegressor(max_depth=max_depth,
-                          learning_rate=learning_rate,
-                          n_estimators=n_estimators,
-                          random_state=13)
-    model.fit(X, y)
-    return model
-
-def tr_gbr(X, y, max_depth=2, learning_rate=0.1, n_estimators=100):
-    model = GradientBoostingRegressor(max_depth=max_depth,
-                                      learning_rate=learning_rate,
-                                      n_estimators=n_estimators,
-                                      random_state=13)
-    model.fit(X, y)
-    return model
